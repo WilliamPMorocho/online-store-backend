91d84bec8722e51a89296019cd4601b79ec12c83
wmorochom <williammorocho@coopdaquilema.com>
2024-06-10 20:43:23 -0500

    Modelo de la base de datos

---
+++ back-end-inventory-products/src/main/java/com/unir/products/model/pojo/PlataformUser.java
@@ -0,0 +1,44 @@
+package com.unir.products.model.pojo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class PlataformUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id", unique = true)
+	private Long productId;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "price")
+	private double price ;
+	
+	@Column(name = "rating")
+	private Long rating;
+
+	@Column(name = "category_id")
+	private Long categoryId;
+	
+	@Column(name = "images")
+	private String images;
+
+	public void update(ProductDto productDto) {
+		this.name = productDto.getName();
+		this.price = productDto.getPrice();
+		this.rating = productDto.getRating();
+		this.categoryId = productDto.getCategoryId();
+		this.images = productDto.getImages();
+	}
+
+}
---
+++ back-end-inventory-products/src/main/java/com/unir/products/model/pojo/ProductOrderDto.java
@@ -0,0 +1,20 @@
+package com.unir.products.model.pojo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ProductOrderDto {
+	
+	private Long productId;
+	private String name;
+	private double price;
+	private Long rating;
+	private Long categoryId;
+	private String images;
+
+}
